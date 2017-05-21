2a7a191ccec6dfa64f8eb87f8f136eb1bd250771
Valeriy Krygin <valeriy.krygin@gmail.com>
2017-05-21 11:39:12 +0300

    Added reducers to main reducers combine

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import vouchersReducer from './vouchersReducer';
+import viewReducer from './viewReducer';
+
 export default combineReducers({
-  vouchers() {
-    return {};
-  },
+  vouchersReducer,
+  viewReducer,
 });
