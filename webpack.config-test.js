ce68c6d589efb525df427674c3d04a329a1b6c0d
Valeriy Krygin <valeriy.krygin@gmail.com>
2017-05-21 15:59:17 +0300

    Added styles to WebPack test config

--- webpack.config-test.js
+++ webpack.config-test.js
@@ -1,7 +1,11 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   target: 'node',
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+  ],
   module: {
     rules: [{
       enforce: 'pre',
@@ -15,6 +19,19 @@ module.exports = {
       query: {
         presets: ['es2015', 'react']
       }
+    }, {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          'css-loader',
+          'sass-loader',
+          'postcss-loader',
+        ]
+      }),
+    }, {
+      test: /\.svg$/,
+      use: 'file-loader',
     }]
   },
   resolve: {
