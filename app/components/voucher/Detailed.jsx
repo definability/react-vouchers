91dd36de74bb4c56dfeafc29a3a25481f0de1b1c
Valeriy Krygin <valeriy.krygin@gmail.com>
2017-05-22 22:35:30 +0300

    Fixed packages paths in Detailed component

--- app/components/voucher/Detailed.jsx
+++ app/components/voucher/Detailed.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import VoucherForm from './voucher/VoucherForm';
-import { fetchVoucher } from '../actions';
+import VoucherForm from './VoucherForm';
+import { fetchVoucher } from '../../actions';
 
 class Detailed extends Component {
   static propTypes() {
